7abdbdf90aadf1d755aea1b1941bf5c00f1940ce
Pedro Duarte <pedro.duarte@thegrandunion.com>
2019-07-30 19:16:28 +0100

    JS

--- sim.js
+++ sim.js
@@ -9,22 +9,14 @@
 var input = {};
 var spells = [];
 var total = 0;
-var log = true;
+var log = false;
 var start = 0;
 var end = 0;
 
 function simulation(i, player) {
-    if (i > input.simulations) {
-        end = new Date().getTime();
-        console.log( (end - start) / 1000 );
-        $('progress').attr('value', i-1);
-        $('#dps').text( (total / (input.timesecs * (i-1))).toFixed(2) +' dps');
-        return;
-    }
-
     let totaldmg = 0;
-    player.reset();
     let executeperc = input.timesecs * 10 * (100 - input.executeperc);
+    player.reset();
     for(let step = 0; step < input.timesecs * 1000; step += 10) {
         player.step();
         if (player.mh.timer == 0) totaldmg += player.attack(player.mh);
@@ -39,7 +31,7 @@ function simulation(i, player) {
                 continue;
             }
             if (player.bloodthirst && player.bloodthirst.canUse()) {
-                totaldmg += player.cast(player.bloodthirst );
+                totaldmg += player.cast(player.bloodthirst);
                 continue;
             }
             if (player.whirlwind && player.whirlwind.canUse()) {
@@ -51,10 +43,15 @@ function simulation(i, player) {
 
     total += totaldmg;
     
-    if (i % 50 == 0) {
+    if (i % Math.floor(input.simulations / 20) == 0 || i == input.simulations) {
         $('progress').attr('value', i);
         $('#dps').text( (total / (input.timesecs * i)).toFixed(2) +' dps');
-        setTimeout(function() { simulation(i+1, player); }, 0);
+        if (i < input.simulations)
+            setTimeout(function() { simulation(i+1, player); }, 0);
+        else {
+            end = new Date().getTime();
+            console.log( (end - start) / 1000 );
+        }
     }
     else {
         simulation(i+1, player);
@@ -71,21 +68,23 @@ $(document).ready(function() {
             input[prop] = parseInt($(this).val());
             player.base[prop] = input[prop];
         });
-        $('input[type="checkbox"]').each(function() {
+        $('#spells input[type="checkbox"]').each(function() {
             if ($(this).is(':checked'))
                 player[$(this).val().toLowerCase()] = eval("new " + $(this).val() + "(player)");
             else
                 player[$(this).val().toLowerCase()] = null;
         });
+        log = $('input[name="debug"]').is(':checked');
         $('progress').attr('max', input.simulations);
+
         player.target.armor = input.armor;
         player.mh = new Weapon(player, 2.7, 66, 124, WEAPONTYPE.SWORD, false);
         player.oh = new Weapon(player, 1.8, 57, 87, WEAPONTYPE.SWORD, true);
-        player.auras.push({ skill: 5 });
-        player.auras.push({ hit: 6 });
-        player.auras.push({ ap: 800 });
-        player.auras.push({ str: 20 });
-        player.auras.push({ crit: 5 });
+        // player.auras.push({ skill: 5 });
+        // player.auras.push({ hit: 6 });
+        // player.auras.push({ ap: 800 });
+        // player.auras.push({ str: 20 });
+        // player.auras.push({ crit: 5 });
         player.update();
 
         simulation(1, player);
