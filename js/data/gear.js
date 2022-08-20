75663b43a710254133c58d79bc81931b367801bd
Zeb Walker <zebouski@gmail.com>
2022-08-20 03:28:40 -0400

    Add 23206 to slot 2

--- js/data/gear.js
+++ js/data/gear.js
@@ -4249,6 +4249,13 @@ var gear = {
          "source": "Naxx",
          "phase": "6",
          "id": 23041,
+      },
+      {
+         "name": "Mark of the Champion (vs Undead)",
+         "ap": 150,
+         "source": "Naxx",
+         "phase": "6",
+         "id": 23206
       }
    ],
    "ranged": [
