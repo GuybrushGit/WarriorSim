9a3b5bd8566c0df6feec4a6f6f38a7570e188b62
GuybrushGit <turinpt@gmail.com>
2024-03-08 01:28:48 +0000

    New armor values

--- js/ui.js
+++ js/ui.js
@@ -908,6 +908,8 @@ SIM.UI = {
         globalThis.filter_green = storage.filter_green;
         globalThis.filter_blue = storage.filter_blue;
         
+        if (storage.targetbasearmor == 4455) storage.targetbasearmor = 3731;
+        console.log(storage.targetbasearmor);
         for (let prop in storage) {
             view.fight.find('input[name="' + prop + '"]').val(storage[prop]);
             view.fight.find('select[name="' + prop + '"]').val(storage[prop]);
